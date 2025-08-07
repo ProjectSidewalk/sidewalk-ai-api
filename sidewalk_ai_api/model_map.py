2d4b08377b4b43a79454f0780bced0d7513954dc
John O'Meara <80555253+jsomeara@users.noreply.github.com>
2025-08-07 11:08:48 -0700

    Create model_map.py

---
+++ sidewalk_ai_api/model_map.py
@@ -0,0 +1,42 @@
+MODEL_MAP = {
+    "tagger_models": {
+        "crosswalk": {
+            "model_id": "validated-dino-cls-b-crosswalk-tags-best",
+            "training_date": "12-03-2024"
+        },
+        "curbramp": {
+            "model_id": "validated-dino-cls-b-curbramp-tags-best",
+            "training_date": "12-03-2024"
+        },
+        "obstacle": {
+            "model_id": "validated-dino-cls-b-obstacle-tags-best",
+            "training_date": "12-03-2024"
+        },
+        "surfaceproblem": {
+            "model_id": "validated-dino-cls-b-surfaceproblem-tags-best",
+            "training_date": "12-03-2024"
+        }
+    },
+    "validator_models": {
+        "crosswalk": {
+            "model_id": "sidewalk-validator-ai-crosswalk",
+            "training_date": "01-26-2025"
+        },
+        "curbramp": {
+            "model_id": "sidewalk-validator-ai-curbramp",
+            "training_date": "01-26-2025"
+        },
+        "obstacle": {
+            "model_id": "sidewalk-validator-ai-obstacle",
+            "training_date": "01-26-2025"
+        },
+        "surfaceproblem": {
+            "model_id": "sidewalk-validator-ai-surfaceproblem",
+            "training_date": "01-26-2025"
+        },
+        "nocurbramp": {
+            "model_id": "sidewalk-validator-ai-nocurbramp",
+            "training_date": "01-26-2025"
+        }
+    }
+}
